9c1c3b2a80fe6013c235ee4086bfdfc2821269a2
Nikoloza23 <abashia.nikoloza@gmail.com>
2022-02-06 16:15:22 +0400

    react

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Navbar from './components/home/Navbar'
 import {Routes, Route} from 'react-router-dom'
 import Products from './components/products/Products';
 import Product from './components/products/Product';
+import Cart from './components/cart/Cart';
 
 
 function App() {
    return (
      <div className="App">
-    <Navbar />
+    {/* <Navbar />
      <Routes>
       <Route path='/products' element={<Products/>} /> 
       <Route path='/products/:id' element={<Product/>} /> 
-     </Routes>
+      <Route path='/cart' element={<Cart/>} /> 
+     </Routes> */}
+
+     
     </div>
   );
 }
